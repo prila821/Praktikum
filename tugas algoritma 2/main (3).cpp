5215560081ec9ede9b4b02e4ba13d392fcd671fc
prila821 <146154362+prila821@users.noreply.github.com>
2023-09-27 10:29:36 +0700

    Add files via upload

---
+++ tugas algoritma 2/main (3).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	float v,s,t;
+	cout<<"PROGRAM C++ MENGHITUNG KECEPATAN"<<endl;
+	cout<<"-----------------------------------"<<endl;
+	cout<<"Masukan Jarak Tempuh (Km)  : ";
+	cin>>s;
+	cout<<"Masukan Waktu Tempuh (Jam) : ";
+	cin>>t;
+	v=s/t;
+	cout<<"Kecepatan (Km/jam)         : "<<v<<endl;
+}
